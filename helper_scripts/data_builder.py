9b5419728fa9ad0e40510cde76f553646288ea59
Victor Medeiros <victoramedeiros@gmail.com>
2021-10-20 06:20:47 -0300

    upgrading databuilder but could go back later idk

--- helper_scripts/data_builder.py
+++ helper_scripts/data_builder.py
@@ -12,7 +12,9 @@ final_df = pd.DataFrame()
 
 def fill_header(val_do_dia, filename):
     # 777;84;Siqueira/Messejana/P;2001;06/01/2015 09:21:03;2;02-ESTUDANTE ETUFOR;Ida;N;
-    val_do_dia.to_csv('dfs_2015/' + filename, header=['id_onibus','linha','linha_nome','undf','data_hora','undf2','tipo_validacao','sentido','integracao'], index=False)
+    val_do_dia.to_csv('dfs_2015/' + filename, header=
+        ['id_onibus','linha','linha_nome','undf','data_hora','undf2','tipo_validacao','sentido','integracao'], 
+        index=False)
     df = pd.read_csv('dfs_2015/' + filename)
     return df
 
@@ -34,20 +36,15 @@ def week_of_month(tgtdate):
     return (tgtdate - startdate).days //7 + 1
 
 def generate_df(filename):
+    encoding = True
+
     print(filename)
     val = pd.read_csv('./raw-2015/' + filename, sep=';')
     val = val.drop('Unnamed: 9', axis=1)
     val.reset_index(drop=True, inplace=True)
-    # print(val.info())
-    validation_georeffed_do_dia = fill_header(val, filename)
-    # validation_georeffed_do_dia.info()
     
-    #print('Before Zero Drop:', len(validation_georeffed_do_dia))
-    #validation_georeffed_do_dia = validation_georeffed_do_dia[validation_georeffed_do_dia.lat != 0.000000]
-    #validation_georeffed_do_dia = validation_georeffed_do_dia[validation_georeffed_do_dia.lng != 0.000000]
-    #print('After Zero Drop:', len(validation_georeffed_do_dia))
-    # validation_georeffed_do_dia.info()
-    # id_onibus,linha,linha_nome,ano_onibus,data_hora,alg,tipo_validacao,sentido,integracao
+    validation_georeffed_do_dia = fill_header(val, filename)
+    
     validation_georeffed_do_dia['id_onibus'] = validation_georeffed_do_dia['id_onibus'].astype(np.int32)
     validation_georeffed_do_dia['linha'] = validation_georeffed_do_dia['linha'].astype(str)
     validation_georeffed_do_dia['linha_nome'] = validation_georeffed_do_dia['linha_nome'].astype(str)
@@ -56,28 +53,35 @@ def generate_df(filename):
                                                               format='%d/%m/%Y %H:%M:%S', errors='coerce')
     validation_georeffed_do_dia['data_hora'] = validation_georeffed_do_dia['data_hora'].dt.floor('H')
     # validation_georeffed_do_dia['undf2'] = validation_georeffed_do_dia['undf2'].astype(np.int32)s
+
     validation_georeffed_do_dia['tipo_validacao'] = validation_georeffed_do_dia['tipo_validacao'].astype(str)
     validation_georeffed_do_dia['sentido'] = validation_georeffed_do_dia['sentido'].astype(str)
     validation_georeffed_do_dia['integracao'] = validation_georeffed_do_dia['integracao'].astype(str)
-    
+
 
     validation_georeffed_do_dia = pd.DataFrame(
         {'validations_per_hour': validation_georeffed_do_dia.groupby(['linha', 'data_hora']).size()}).reset_index()
 
     validation_georeffed_do_dia['d_semana'] = validation_georeffed_do_dia.apply(lambda row: row.data_hora.dayofweek, axis=1)
 
-    validation_georeffed_do_dia = encode(validation_georeffed_do_dia, )
-
-    validation_georeffed_do_dia['hour_sin'] = np.sin(2 * np.pi * validation_georeffed_do_dia['data_hora'].dt.hour/23.0)
-    validation_georeffed_do_dia['hour_cos'] = np.cos(2 * np.pi * validation_georeffed_do_dia['data_hora'].dt.hour/23.0)
-    validation_georeffed_do_dia["hora"] = validation_georeffed_do_dia['data_hora'].dt.strftime('%H')
     validation_georeffed_do_dia["d_mes"] = validation_georeffed_do_dia['data_hora'].dt.day
+
     validation_georeffed_do_dia["d_ano"] = validation_georeffed_do_dia['data_hora'].dt.dayofyear
+
     validation_georeffed_do_dia["mes"] = validation_georeffed_do_dia['data_hora'].dt.month
+
     validation_georeffed_do_dia["semana_do_mes"] = validation_georeffed_do_dia['data_hora'].apply(week_of_month)
 
-    print(len(validation_georeffed_do_dia))
-    print(validation_georeffed_do_dia)
+    validation_georeffed_do_dia["hora"] = validation_georeffed_do_dia['data_hora'].dt.strftime('%H').astype(int)
+
+    if (encoding == True):
+        validation_georeffed_do_dia['d_mes'] = encode(validation_georeffed_do_dia, 'd_mes', 31)
+        validation_georeffed_do_dia['d_semana'] = encode(validation_georeffed_do_dia, 'd_semana', 6)
+        validation_georeffed_do_dia['d_ano'] = encode(validation_georeffed_do_dia, 'd_ano', 366)
+        validation_georeffed_do_dia['mes'] = encode(validation_georeffed_do_dia, 'mes', 12)
+        validation_georeffed_do_dia['semana_do_mes'] = encode(validation_georeffed_do_dia, 'semana_do_mes', 4)
+        validation_georeffed_do_dia['hora'] = encode(validation_georeffed_do_dia, 'hora', 23)
+
     global final_df
     final_df = pd.concat([final_df, validation_georeffed_do_dia], ignore_index=True)
     print(len(final_df))
